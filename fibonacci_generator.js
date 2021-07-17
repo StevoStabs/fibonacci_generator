8f6006f2c46b8692cbde3e6efaf999045a400a60
sstabile <stevenstabile@gmail.com>
2021-07-17 00:40:46 -0400

    Create fibonacci_generator.js
    Add fibonacci_generator method and a few tests.

---
+++ fibonacci_generator.js
@@ -0,0 +1,25 @@
+/*
+   Method takes an integer input n and prints n digits 
+   in fibonacci sequence. 
+
+   Steven Stabile
+*/
+
+const fibonacci_generator = n => {
+    
+    for(let i=0; i<n; i++) {
+        
+        
+    }
+    
+}
+
+// Tests
+// Expect 0 1 1 2 3
+fibonacci_generator(5);
+
+// Expect 0 1 1 
+fibonacci_generator(3);
+
+// Expect 0 1 1 2
+fibonacci_generator(4);
